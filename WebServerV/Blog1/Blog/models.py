c008e01a27266a1f7f4c142b7dd32359e770153c
callor <callor@callor.com>
2022-04-24 11:52:41 +0900

    Dj

--- WebServerV/Blog1/Blog/models.py
+++ WebServerV/Blog1/Blog/models.py
@@ -7,7 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     comment = models.TextField()
 
-    create_at = models.DateTimeField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     # '관리자 화면에서 post 리스트 를 보일때 [PK]title 값으로 제목을 표시
     # 지금 Post table 은 pk 라는 이름 으로 PK 가 설정 되어 있다.
--- WebServerV/Blog1/Blog1/settings.py
+++ WebServerV/Blog1/Blog1/settings.py
@@ -106,11 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'ko-kr'
-
 TIME_ZONE = 'Asia/Seoul'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
